8272c43e390689c40edc04aafccca38c11c4b368
JustinRowe15 <JustinRowe15@fullsail.edu>
2013-02-28 19:01:03 -0500

    Initial commit for week 4.

---
+++ AOC2Week4/AOC2Week4/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  AOC2Week4
+//
+//  Created by Justin Rowe on 2/28/13.
+//  Copyright (c) 2013 Justin Rowe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ AOC2Week4/AOC2Week4/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AOC2Week4
+//
+//  Created by Justin Rowe on 2/28/13.
+//  Copyright (c) 2013 Justin Rowe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
